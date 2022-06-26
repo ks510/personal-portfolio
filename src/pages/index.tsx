9201a48c7bc1492adcacc3c55adb44f158dec09e
Karen <ksuen.dev@gmail.com>
2022-06-26 02:41:53 +0100

    ✨ UPDATE page title and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <div className="background-beams grid place-items-center h-screen">
-      <div className="bg-black px-24 py-16 bg-black/[.5] backdrop-blur-xl backdrop-saturate-[180%] rounded-xl border border-gray-800 text-center">
-        <p className="text-2xl font-medium mb-4">Karen&apos;s Portfolio</p>
-        <h1 className="text-5xl font-semibold text-lavender">
-          🚀 Coming soon...
-        </h1>
+    <>
+      <Head>
+        <title>Karen&apos;s Portfolio</title>
+      </Head>
+      <div className="background-beams grid place-items-center h-screen">
+        <div className="bg-black px-24 py-16 bg-black/[.5] backdrop-blur-xl backdrop-saturate-[180%] rounded-xl border border-gray-800 text-center">
+          <p className="text-2xl font-medium mb-4">Karen&apos;s Portfolio</p>
+          <h1 className="text-5xl font-semibold text-lavender">
+            🚀 Coming soon...
+          </h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
