df6d6087c514a2d5e848f3566c9158ea7e6929e0
Karen <ksuen.dev@gmail.com>
2022-06-26 01:40:10 +0100

    🍱 ADD background light beams image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="rounded-xl grid place-items-center h-screen">
+    <div className="background-beams rounded-xl grid place-items-center h-screen">
       <h1 className="text-5xl text-indigo-100">Coming soon...🚀</h1>
     </div>
   );
