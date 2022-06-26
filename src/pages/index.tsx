3e32cbe2d1dcb545359d39c4e8e8aeaaaf4e11cf
Karen <ksuen.dev@gmail.com>
2022-06-26 03:20:15 +0100

    📱 MAKE responsive on all devices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,12 @@ const Home: NextPage = () => {
       <Head>
         <title>Karen&apos;s Portfolio</title>
       </Head>
-      <div className="background-beams grid place-items-center h-screen">
-        <div className="bg-black px-24 py-16 bg-black/[.5] backdrop-blur-xl backdrop-saturate-[180%] rounded-xl border border-gray-800 text-center">
-          <p className="text-2xl font-medium mb-4">Karen&apos;s Portfolio</p>
-          <h1 className="text-5xl font-semibold text-lavender">
+      <div className="background-beams h-screen grid place-items-center">
+        <div className="bg-black py-10 px-10 md:px-24 md:py-16 bg-black/[.5] backdrop-blur-xl backdrop-saturate-[180%] rounded-xl border border-gray-800 text-center">
+          <p className="text-lg md:text-2xl font-medium mb-4">
+            Karen&apos;s Portfolio
+          </p>
+          <h1 className="text-2xl md:text-5xl font-semibold text-lavender">
             🚀 Coming soon...
           </h1>
         </div>
